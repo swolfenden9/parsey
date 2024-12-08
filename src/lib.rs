53262e48613e7ab48fa31cc38caf2a38e0afae31
Sol Wolf <wolfendenbuild@gmail.com>
2024-12-08 20:51:35 +1000

    add PeekableParser usage to documentation and bump version

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,61 @@
 //! # Parsey
 //!
 //! `parsey` is a lightweight, `no_std` framework for creating custom parsers and abstract syntax trees (ASTs).
-//! It provides two key traits: [`Parser`][crate::Parser] and [`Ast`][crate::Ast], which together form the foundation
+//! It provides two key traits: [`Parser`] and [`Ast`], which together form the foundation
 //! for building parsers and representing the structure of parsed data.
 //!
 //! ## Key Features
 //! - **Generic Parsing Framework:** Abstracts the process of parsing tokens into structured data.
-//! - **Customizable AST Nodes:** Easily define nodes of your AST by implementing the [`Ast`][crate::Ast] trait.
+//! - **Customizable AST Nodes:** Easily define nodes of your AST by implementing the [`Ast`] trait.
 //! - **Integration with `no_std`:** Ideal for embedded or constrained environments.
 //!
 //! ## Getting Started
 //!
 //! ### Step 1: Implement the `Parser` Trait
-//! Define a struct that will serve as your parser. This struct must implement the [`Parser`][crate::Parser] trait,
+//! Define a struct that will serve as your parser. This struct must implement the [`Parser`] trait,
 //! which processes tokens and produces an AST.
 //!
-//! ```rust
-//! use parsey::{Parser, Ast};
+//! ```rust,ignore
+//! use parsey::{Ast, Parser, PeekableParser};
 //!
-//! #[derive(Debug)]
-//! pub enum Token {
+//! #[derive(Debug, PartialEq)]
+//! pub enum MyToken {
 //!     Zero,
 //!     One,
 //! }
 //!
-//! #[derive(Debug)]
+//! #[derive(Debug, PartialEq)]
 //! pub struct MyError;
 //!
 //! pub struct MyParser {
-//!     tokens: Vec<Token>,
+//!     tokens: Vec<MyToken>,
 //! }
 //!
 //! impl MyParser {
-//!     pub fn new(mut tokens: Vec<Token>) -> Self {
+//!     pub fn new(mut tokens: Vec<MyToken>) -> Self {
 //!         tokens.reverse();
 //!         Self { tokens }
 //!     }
 //! }
 //!
-//! impl parsey::Parser<Token, MyError> for MyParser {
-//!     type Root = Program;
+//! impl Parser<MyToken, MyError> for MyParser {
+//!     type Root = Root;
+//!
+//!     fn expect(
+//!         peekable_parser: &mut PeekableParser<Self, MyToken, MyError>,
+//!         token: MyToken,
+//!     ) -> Result<(), MyError> {
+//!         if peekable_parser.peek() == Some(&token) {
+//!             peekable_parser.next();
+//!             Ok(())
+//!         } else {
+//!             Err(MyError)
+//!         }
+//!     }
 //! }
 //!
 //! impl Iterator for MyParser {
-//!     type Item = Token;
+//!     type Item = MyToken;
 //!
 //!     fn next(&mut self) -> Option<Self::Item> {
 //!         self.tokens.pop()
@@ -52,48 +64,48 @@
 //! ```
 //!
 //! ### Step 2: Define the AST Nodes
-//! Create the structure for your AST by implementing the [`Ast`][crate::Ast] trait for each node.
+//! Create the structure for your AST by implementing the [`Ast`] trait for each node.
 //! The root node must match the type defined in `Parser::Root`.
 //!
-//! ```rust
-//! #[derive(Debug)]
-//! pub struct Program(Vec<Statement>);
+//! ```rust,ignore
+//! #[derive(Debug, PartialEq)]
+//! pub struct Root(Vec<TwoBit>);
 //!
-//! #[derive(Debug)]
-//! pub enum Statement {
+//! #[derive(Debug, PartialEq)]
+//! pub enum TwoBit {
 //!     ZeroZero,
 //!     ZeroOne,
 //!     OneZero,
 //!     OneOne,
 //! }
 //!
-//! impl parsey::Ast<Token, MyError> for Program {
-//!     fn parse<P>(parser: &mut std::iter::Peekable<P>) -> Result<Self, MyError>
+//! impl Ast<MyToken, MyError> for Root {
+//!     fn parse<P>(parser: &mut PeekableParser<P, MyToken, MyError>) -> Result<Self, MyError>
 //!     where
-//!         P: parsey::Parser<Token, MyError>,
+//!         P: Parser<MyToken, MyError>,
 //!     {
-//!         let mut statements = vec![];
+//!         let mut two_bits = vec![];
 //!         while parser.peek().is_some() {
-//!             statements.push(Statement::parse(parser)?);
+//!             two_bits.push(TwoBit::parse(parser)?);
 //!         }
-//!         Ok(Self(statements))
+//!         Ok(Self(two_bits))
 //!     }
 //! }
 //!
-//! impl parsey::Ast<Token, MyError> for Statement {
-//!     fn parse<P>(parser: &mut std::iter::Peekable<P>) -> Result<Self, MyError>
+//! impl parsey::Ast<MyToken, MyError> for TwoBit {
+//!     fn parse<P>(parser: &mut PeekableParser<P, MyToken, MyError>) -> Result<Self, MyError>
 //!     where
-//!         P: parsey::Parser<Token, MyError>,
+//!         P: parsey::Parser<MyToken, MyError>,
 //!     {
 //!         match parser.next() {
-//!             Some(Token::Zero) => match parser.next() {
-//!                 Some(Token::Zero) => Ok(Statement::ZeroZero),
-//!                 Some(Token::One) => Ok(Statement::ZeroOne),
+//!             Some(MyToken::Zero) => match parser.next() {
+//!                 Some(MyToken::Zero) => Ok(TwoBit::ZeroZero),
+//!                 Some(MyToken::One) => Ok(TwoBit::ZeroOne),
 //!                 _ => Err(MyError),
 //!             },
-//!             Some(Token::One) => match parser.next() {
-//!                 Some(Token::Zero) => Ok(Statement::OneZero),
-//!                 Some(Token::One) => Ok(Statement::OneOne),
+//!             Some(MyToken::One) => match parser.next() {
+//!                 Some(MyToken::Zero) => Ok(TwoBit::OneZero),
+//!                 Some(MyToken::One) => Ok(TwoBit::OneOne),
 //!                 _ => Err(MyError),
 //!             },
 //!             _ => Err(MyError),
@@ -105,15 +117,15 @@
 //! ### Step 3: Parse Tokens
 //! Use your parser to parse a sequence of tokens into an AST.
 //!
-//! ```rust
+//! ```rust,ignore
 //! fn main() {
-//!     let tokens = vec![Token::One, Token::Zero, Token::One, Token::Zero];
+//!     use MyToken::{One, Zero};
+//!     use TwoBit::{OneOne, OneZero, ZeroOne, ZeroZero};
+//!
+//!     let tokens = vec![Zero, Zero, Zero, One, One, Zero, One, One];
 //!     let parser = MyParser::new(tokens);
-//!     let ast = parsey::Parser::parse(parser);
-//!     match ast {
-//!         Ok(ast) => println!("Parsed AST: {:?}", ast),
-//!         Err(e) => eprintln!("Parsing error: {:?}", e),
-//!     }
+//!     let ast = parser.parse().unwrap();
+//!     assert_eq!(ast, Root(vec![ZeroZero, ZeroOne, OneZero, OneOne]));
 //! }
 //! ```
 
@@ -162,12 +174,6 @@ pub trait Parser<Token, Error>: Iterator<Item = Token> + Sized {
     /// # Returns
     /// - `Ok(())` if the token matches the expected token
     /// - `Err(Error)` if the token does not match the expected token
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    ///
-    /// ```
     fn expect(
         peekable_parser: &mut PeekableParser<Self, Token, Error>,
         expected: Token,
@@ -206,16 +212,18 @@ where
     /// - `Err(Error)` if the token does not match the expected token
     ///
     /// # Examples
-    ///
-    /// ```rust
-    ///
-    /// ```
     pub fn expect(&mut self, expected: Token) -> Result<(), Error> {
         P::expect(self, expected)
     }
+}
 
-    /// Advances to the next token and returns it.
-    pub fn next(&mut self) -> Option<Token> {
+impl<P, Token, Error> Iterator for PeekableParser<P, Token, Error>
+where
+    P: Parser<Token, Error>,
+{
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
         self.inner.next()
     }
 }
--- tests/twobit.rs
+++ tests/twobit.rs
@@ -1,25 +1,14 @@
-# Parsey
-
-`parsey` is a lightweight, `no_std` framework for creating custom parsers and abstract syntax trees (ASTs).
-
-It provides two key traits: [`Parser`] and [`Ast`], which together form the foundation
-for building parsers and representing the structure of parsed data.
-
-## Key Features
-
-- **Generic Parsing Framework:** Abstracts the process of parsing tokens into structured data.
-- **Customizable AST Nodes:** Easily define nodes of your AST by implementing the [`Ast`] trait.
-- **Integration with `no_std`:** Ideal for embedded or constrained environments.
-
-## Getting Started
-
-### Step 1: Implement the `Parser` Trait
-
-Define a struct that will serve as your parser. This struct must implement the [`Parser`] trait,
-which processes tokens and produces an AST.
-
-```rust
 use parsey::{Ast, Parser, PeekableParser};
+
+fn main() {
+    use MyToken::{One, Zero};
+    use TwoBit::{OneOne, OneZero, ZeroOne, ZeroZero};
+
+    let tokens = vec![Zero, Zero, Zero, One, One, Zero, One, One];
+    let parser = MyParser::new(tokens);
+    let ast = parser.parse().unwrap();
+    assert_eq!(ast, Root(vec![ZeroZero, ZeroOne, OneZero, OneOne]));
+}
 
 #[derive(Debug, PartialEq)]
 pub enum MyToken {
@@ -64,14 +53,7 @@ impl Iterator for MyParser {
         self.tokens.pop()
     }
 }
-```
 
-### Step 2: Define the AST Nodes
-
-Create the structure for your AST by implementing the [`Ast`] trait for each node.
-The root node must match the type defined in `Parser::Root`.
-
-```rust
 #[derive(Debug, PartialEq)]
 pub struct Root(Vec<TwoBit>);
 
@@ -116,23 +98,3 @@ impl parsey::Ast<MyToken, MyError> for TwoBit {
         }
     }
 }
-```
-
-### Step 3: Parse Tokens
-
-Use your parser to parse a sequence of tokens into an AST.
-
-```rust
-fn main() {
-    use MyToken::{One, Zero};
-    use TwoBit::{OneOne, OneZero, ZeroOne, ZeroZero};
-
-    let tokens = vec![Zero, Zero, Zero, One, One, Zero, One, One];
-    let parser = MyParser::new(tokens);
-    let ast = parser.parse().unwrap();
-    assert_eq!(ast, Root(vec![ZeroZero, ZeroOne, OneZero, OneOne]));
-}
-```
-
-[`Parser`]: https://docs.rs/parsey/latest/parsey/trait.Parser.html
-[`Ast`]: https://docs.rs/parsey/latest/parsey/trait.Ast.html
