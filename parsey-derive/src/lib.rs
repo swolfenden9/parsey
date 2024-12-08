052e1a9e81df40ca899c245dd4ca8ef81be8f2ef
Sol Wolf <wolfendenbuild@gmail.com>
2024-12-09 08:44:55 +1000

    remove parsey-derive

--- parsey-derive/src/lib.rs
+++
@@ -1,8 +0,0 @@
-use proc_macro::TokenStream;
-
-mod ast;
-
-#[proc_macro_derive(Ast, attributes(ast))]
-pub fn derive_ast(input: TokenStream) -> TokenStream {
-    ast::ast_impl(input.into()).into()
-}
